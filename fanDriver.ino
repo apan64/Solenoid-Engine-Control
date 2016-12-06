07cb63361186e2a5d8755ab3c437eeb606033e5e
apan64 <andrew.pan@students.olin.edu>
2016-12-06 11:15:13 -0500

    Fan driving code

---
+++ fanDriver.ino
@@ -0,0 +1,21 @@
+int thermo1 = A0;
+int thermo2 = A1;
+int thermo3 = A2;
+int thermo4 = A3;
+int thermo5 = A4;
+int thermo6 = A5;
+int fanDriver = 11;
+void setup() {
+  pinMode(thermo1, INPUT);
+  pinMode(thermo2, INPUT);
+  pinMode(thermo3, INPUT);
+  pinMode(thermo4, INPUT);
+  pinMode(thermo5, INPUT);
+  pinMode(thermo6, INPUT);
+  pinMode(fanDriver, OUTPUT);
+}
+
+void loop() {
+  thermoAverage = (analogRead(thermo1) + analogRead(thermo2) + analogRead(thermo3) + analogRead(thermo4) + analogRead(thermo5) + analogRead(thermo6)) / 6;
+  analogWrite(fanDriver, 255 * (thermoAverage / 200));
+}
